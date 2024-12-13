038df6b38f0a02b05c9472bac1d1a050576ea3ac
kauu1 <samukbore99@gmail.com>
2024-12-13 18:13:06 -0300

    chaging feedback to the user

--- knapsack/check_exercise.jl
+++ knapsack/check_exercise.jl
@@ -13,21 +13,29 @@ function check_exercise(model, n, capacity, profits, weights, exercise)
     n_test = size(tests)[1]
     
     correct_tests = 0
-
+    wrong_answer_found = 0
     for i in 1:n_test
         status = model(n,capacity, profits,weights, tests[i])
         status_int = (status == INFEASIBLE) ? 1 : 0
         
         if status_int == check[i]
             correct_tests += 1
+        elseif wrong_answer_found == 0
+            println("\033[31mErro in test: ",tests[i])
+            if(check[i] == 0)
+                println("Expected: OPTIMAL, Got: ", status)
+            else
+                println("Expected: INFEASIBLE, Got: ", status) 
+            end
+            wrong_answer_found = 1
         end
 
     end
 
     if correct_tests == n_test
-        print("\033[32mAll Tests Passed!!")
+        print("\n\033[32mAll Tests Passed!!")
     else
-        print("\033[31mWrong Answer - ",correct_tests, "/", n_test," tests passed.")
+        print("\n\033[31mWrong Answer - ",Int(round(correct_tests*100/n_test)),"% of all tests passed.")
     end
     
 end
